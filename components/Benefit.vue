599b507f984fe7af9c9c9ddc34406fcf26dfd34f
halawaoji97 <ozidhalawa97@gmail.com>
2022-08-07 08:03:48 +0700

    add: prettier

--- components/Benefit.vue
+++ components/Benefit.vue
@@ -1,8 +1,12 @@
 <template>
   <section class="w-full pt-0 lg:py-20" id="benefits">
-    <div class="container px-4 md:px-16 mx-auto" data-aos="fade-up" data-aos-duration="2000">
+    <div
+      class="container mx-auto px-4 md:px-16"
+      data-aos="fade-up"
+      data-aos-duration="2000"
+    >
       <div class="text-center">
-        <h5 class="text-base text-red-primary font-semibold mb-2">
+        <h5 class="mb-2 text-base font-semibold text-red-primary">
           OUR SUPER BENEFITS
         </h5>
         <h2 class="text-3xl font-bold text-black-primary">
@@ -11,43 +15,62 @@
       </div>
 
       <div
-        class="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 gap-10 lg:gap-20 lg:grid-rows-1 mt-12">
-        <div class="rounded-xl smooth-shadow mx-auto p-6 max-w-md w-full md:max-w-full">
-          <div class="w-16 h-16 rounded-full ring-2 ring-white-primary bg-grey-secondary">
+        class="mt-12 grid grid-cols-1 grid-rows-3 gap-10 md:grid-cols-2 md:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 lg:gap-20"
+      >
+        <div
+          class="smooth-shadow mx-auto w-full max-w-md rounded-xl p-6 md:max-w-full"
+        >
+          <div
+            class="h-16 w-16 rounded-full bg-grey-secondary ring-2 ring-white-primary"
+          >
             <RealTime class="h-full w-full p-2" />
           </div>
-          <h6 class="text-xl mt-6 font-bold text-black-primary leading-10">
+          <h6 class="mt-6 text-xl font-bold leading-10 text-black-primary">
             Real-time solution
           </h6>
-          <p class="text-grey-primary text-base font-medium leading-relaxed mt-1">
+          <p
+            class="mt-1 text-base font-medium leading-relaxed text-grey-primary"
+          >
             Your application will run
             <br class="hidden md:block" />
             in real time 24 hours
           </p>
         </div>
 
-        <div class="rounded-xl smooth-shadow mx-auto p-6 max-w-sm w-full md:max-w-full">
-          <div class="w-16 h-16 rounded-full ring-2 ring-white-primary bg-grey-secondary">
+        <div
+          class="smooth-shadow mx-auto w-full max-w-sm rounded-xl p-6 md:max-w-full"
+        >
+          <div
+            class="h-16 w-16 rounded-full bg-grey-secondary ring-2 ring-white-primary"
+          >
             <SEO class="h-full w-full p-2" />
           </div>
-          <h6 class="text-xl mt-6 font-bold text-black-primary leading-10">
+          <h6 class="mt-6 text-xl font-bold leading-10 text-black-primary">
             SEO Optimization
           </h6>
-          <p class="text-grey-primary text-base font-medium leading-relaxed mt-1">
+          <p
+            class="mt-1 text-base font-medium leading-relaxed text-grey-primary"
+          >
             We make sure your website
             <br class="hidden md:block" />
             is indexed by google search
           </p>
         </div>
 
-        <div class="rounded-xl smooth-shadow mx-auto p-6 max-w-sm w-full md:max-w-full">
-          <div class="w-16 h-16 rounded-full ring-2 ring-white-primary bg-grey-secondary">
+        <div
+          class="smooth-shadow mx-auto w-full max-w-sm rounded-xl p-6 md:max-w-full"
+        >
+          <div
+            class="h-16 w-16 rounded-full bg-grey-secondary ring-2 ring-white-primary"
+          >
             <Security class="h-full w-full p-2" />
           </div>
-          <h6 class="text-xl mt-6 font-bold text-black-primary leading-10">
+          <h6 class="mt-6 text-xl font-bold leading-10 text-black-primary">
             Security System
           </h6>
-          <p class="text-grey-primary text-base font-medium leading-relaxed mt-1">
+          <p
+            class="mt-1 text-base font-medium leading-relaxed text-grey-primary"
+          >
             High level of security & tested
             <br class="hidden md:block" />
             against attacks from outside
